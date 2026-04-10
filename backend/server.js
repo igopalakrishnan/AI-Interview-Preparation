66b59f78c68af91c84c3d9410a90e7584940f262
gopalakrishnan <kingkrishna809@gmail.com>
2026-04-10 22:33:20 +0530

    update frontend url to backend

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,10 @@ const app = express();
 //Middleware to handle CORS
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "https://ai-interviews-preparation.netlify.app",
+    origin: [
+      "http://localhost:5173",
+      "https://ai-interviews-preparation.netlify.app",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   }),
