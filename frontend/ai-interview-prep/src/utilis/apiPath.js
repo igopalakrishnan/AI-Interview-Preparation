de857f90facd06f159c844effed9178342c6863c
gopalakrishnan <kingkrishna809@gmail.com>
2026-04-10 22:24:50 +0530

    correction path

--- frontend/ai-interview-prep/src/utilis/apiPath.js
+++ frontend/ai-interview-prep/src/utilis/apiPath.js
@@ -4,7 +4,7 @@ export const API_PATHS = {
   AUTH: {
     REGISTER: "/api/auth/register",
     LOGIN: "/api/auth/login",
-    GET_PROFILE: "api/auth/profile",
+    GET_PROFILE: "/api/auth/profile",
   },
 
   IMAGE: {
