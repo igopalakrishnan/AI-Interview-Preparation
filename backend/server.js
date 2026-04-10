1aa01d51b62c91250c016cddb0f59a4f6276ef5f
gopalakrishnan <kingkrishna809@gmail.com>
2026-04-10 21:30:50 +0530

    update cors

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const app = express();
 //Middleware to handle CORS
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   }),
